aeb272b33af344fa01240919cce6e7c56280f3b3
Greg Garvey <ggarvey@stanford.edu>
2017-04-28 13:24:31 -0700

    added megamenu

---
+++ js/stanford_soe_regions_megamenu.js
@@ -0,0 +1,8 @@
+(function ($) {
+  $(document).ready(function(){
+    //Do Stuff...
+    //var megamenu = '<div>Yo, I\'m Stuff</div>';
+    //$('#fullwidth-top').append(megamenu);
+    //TODO: hook topics link to  dropdown action
+  });
+})(jQuery);
