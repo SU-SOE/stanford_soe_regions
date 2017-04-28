98877ffd07a1c478b24ac876fb4004ca898429bd
Greg Garvey <ggarvey@stanford.edu>
2017-04-28 16:42:17 -0700

    adding overlay

--- js/stanford_soe_regions_megamenu.js
+++ js/stanford_soe_regions_megamenu.js
@@ -2,12 +2,13 @@
   $(document).ready(function(){
     //Do Stuff...
     //var megamenu = '<div class="dm-megamenu">Yo, I\'m Stuff</div>';
-    $('#digital-magazine-megamenu').insertAfter('#fullwidth-top');$('.dm-megamenu').css('display', 'none');
-    $('#digital-magazine-megamenu').css('display', 'none');
+    //$('#digital-magazine-megamenu').insertAfter('#fullwidth-top');$('.dm-megamenu').css('display', 'none');
+    var megamenu = $('#digital-magazine-megamenu .container');
+    $(megamenu).css('display', 'none');
     //TODO: hook topics link to  dropdown action
     $('.menu-name-menu-magazine ul a').first().click(function(e) {
       e.preventDefault();
-      $('#digital-magazine-megamenu').slideToggle( "slow", function() {
+      $('#digital-magazine-megamenu .container').slideToggle( "slow", function() {
         // Animation complete.
       });
     });
