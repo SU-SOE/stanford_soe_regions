baa3bdf9320e3fee6ab2f1619fd458fa5ba5f3c4
Greg Garvey <ggarvey@stanford.edu>
2017-04-28 18:21:06 -0700

    interaction mods

--- js/stanford_soe_regions_megamenu.js
+++ js/stanford_soe_regions_megamenu.js
@@ -1,14 +1,12 @@
 (function ($) {
   $(document).ready(function(){
     //Do Stuff...
-    //var megamenu = '<div class="dm-megamenu">Yo, I\'m Stuff</div>';
-    //$('#digital-magazine-megamenu').insertAfter('#fullwidth-top');$('.dm-megamenu').css('display', 'none');
     var megamenu = $('#digital-magazine-megamenu .container');
     $(megamenu).css('display', 'none');
-    //TODO: hook topics link to  dropdown action
-    $('.menu-name-menu-magazine ul a').first().click(function(e) {
+    var topics = $('.menu-name-menu-magazine ul a').first();
+    $(topics).click(function(e) {
       e.preventDefault();
-      $('#digital-magazine-megamenu .container').slideToggle( "slow", function() {
+      $(megamenu).slideToggle( "slow", function() {
         // Animation complete.
       });
     });
