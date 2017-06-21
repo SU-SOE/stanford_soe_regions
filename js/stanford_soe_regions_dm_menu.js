c36e1049be57ed5ae08ffda897a1a803004cc574
Greg Garvey <ggarvey@stanford.edu>
2017-06-21 15:54:50 -0700

    added placeholder removal and close div

--- js/stanford_soe_regions_dm_menu.js
+++ js/stanford_soe_regions_dm_menu.js
@@ -1,12 +1,16 @@
 (function ($) {
   $(document).ready(function(){
-    $(window).resize( function() {
+    $(window).on('load resize', function() {
+      //Have some variables.
       var windowSize = $(window).width();
       var randi = $('#digital-magazine-menu .region-digital-magazine-menu .block-menu-block h2');
       var dm_search = $('#digital-magazine-menu .block-stanford-search-api');
       var dm_search_input = $('#digital-magazine-menu .block-stanford-search-api input[type="text"]');
+      var searchCloseMarkup = '<div class="dm-search-close">| X</div>';
+
       if (windowSize < 768) {
         randi.html("Research <br>& Ideas");
+        dm_search_input.attr("placeholder", "");
         var dm_height = $('#digital-magazine-menu').height();
         dm_search_input.on('focus', function() {
           dm_search.css({
@@ -16,6 +20,9 @@
             'width' : '100%',
             'height' : dm_height
           });
+          if ($('.dm-search-close').length == 0) {
+            dm_search.append(searchCloseMarkup);
+          }
         });
         dm_search_input.blur(function() {
           dm_search.css({
@@ -25,10 +32,12 @@
             'width' : 'auto',
             'height' : 'auto'
           });
+          $('.dm-search-close').remove();
         });
       }
       if (windowSize > 767) {
         randi.html("Research & Ideas");
+        dm_search_input.attr("placeholder", "Search");
         dm_search_input.unbind();
         dm_search.css({
           'z-index' : '',
@@ -37,6 +46,7 @@
           'width' : 'auto',
           'height' : 'auto'
         });
+        $('.dm-search-close').remove();
       }
     });
   });
