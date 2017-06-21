d59c53cd95711ec36fe2f8f7f5944b17aa22758b
Greg Garvey <ggarvey@stanford.edu>
2017-06-21 13:45:14 -0700

    initial js for search in mobile

--- js/stanford_soe_regions_dm_menu.js
+++ js/stanford_soe_regions_dm_menu.js
@@ -1,5 +1,36 @@
 (function ($) {
   $(document).ready(function(){
-    //Do Stuff...
+    $(window).resize( function() {
+      var windowSize = $(window).width();
+      var randi = $('#digital-magazine-menu .region-digital-magazine-menu .block-menu-block h2');
+      var dm_search = $('#digital-magazine-menu .block-stanford-search-api');
+      var dm_search_input = $('#digital-magazine-menu .block-stanford-search-api input[type="text"]');
+      console.log(windowSize);
+      if (windowSize < 753) {
+        randi.html("Research <br>& Ideas");
+        var dm_height = $('#digital-magazine-menu').height();
+        dm_search_input.on('click', function() {
+          dm_search.css({
+            'z-index' : '9999',
+            'position' : 'absolute',
+            'background' : '#00ECE9',
+            'width' : '100%',
+            'height' : dm_height
+          });
+        });
+        dm_search_input.blur(function() {
+          dm_search.css({
+            'z-index' : '',
+            'position' : 'relative',
+            'background' : 'none',
+            'width' : 'auto',
+            'height' : 'auto'
+          });
+        });
+      }
+      if (windowSize > 752) {
+        randi.html("Research & Ideas");
+      }
+    });
   });
 })(jQuery);
